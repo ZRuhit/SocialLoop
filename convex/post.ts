2211c3c86f392a52ad2ff426d5d81bad51e62d43
ZRuhit <zubayerhaque@iut-dhaka.edu>
2025-07-30 16:00:06 +0600

    Add post.ts and styles for new post creation

---
+++ convex/post.ts
@@ -0,0 +1,49 @@
+import {v} from 'convex/values';
+import { mutation } from './_generated/server';
+
+export const generateUploadUrl = mutation(async(ctx) =>{
+  const identity= await ctx.auth.getUserIdentity();
+  if(!identity) throw new Error("Unauthorized");
+  return await ctx.storage.generateUploadUrl();
+});
+
+export const createPost= mutation({
+  args: {
+    caption: v.optional(v.string()),
+    StorageId:v.id("_storage"),
+  },
+
+  handler:async(ctx, args) => {
+   const identity= await ctx.auth.getUserIdentity();
+  if(!identity) throw new Error("Unauthorized");
+
+    const currentUser= await ctx.db.query("users").withIndex(
+        "by_clerk_id", (q) => q.eq("clerkId", identity.subject)
+    ).first()
+
+    if(!currentUser) throw new Error("User not found");
+
+    const imageUrl = await ctx.storage.getUrl(args.StorageId);
+    if (!imageUrl) {
+        throw new Error("Failed to retrieve image URL");
+    }
+
+    //create post
+
+   const postId = await ctx.db.insert("posts", {
+        userId: currentUser._id,
+        imageUrl,
+        storageId: args.StorageId,
+        caption: args.caption || "",
+        likes:0,
+        comments: 0
+    })
+
+    //increment user posts count by 1
+    await ctx.db.patch(currentUser._id, {
+      posts: currentUser.posts + 1
+    });
+
+    return postId;
+}
+})
